e0e2d4f6c98e54321dd6f220a55ebca5740dbe98
Ryan Powell <ryangarethpowell@gmail.com>
2021-12-05 12:40:59 +0000

    Github Actions build support to GH-PAGES

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+// module.exports = {
+//     basePath: 'https://ryandev.github.io/RoadRash2-PasscodeGenerator/',
+//     assetPrefix: 'https://ryandev.github.io/RoadRash2-PasscodeGenerator/RoadRash2-PasscodeGenerator/',
+// }
+
+// const isProd = process.env.NODE_ENV === 'production';
+const isProd = false;
+const githubRepoName = 'RoadRash2-PasscodeGenerator';
+const githubAccountName = 'ryandev';
+
+
+// module.exports = {
+//   assetPrefix: isProd ? `/${githubRepoName}/` : '',
+//   basePath: isProd ? `/https://${githubAccountName}/${githubRepoName}/` : '',
+// };
+
 module.exports = {
-    basePath: '/RoadRash2-PasscodeGenerator',
-    assetPrefix: '/RoadRash2-PasscodeGenerator/',
+    assetPrefix: '',
 }
