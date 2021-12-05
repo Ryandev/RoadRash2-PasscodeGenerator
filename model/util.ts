50d156254860d757f2e3fa91ab9eb7401065b51c
Ryan Powell <ryangarethpowell@gmail.com>
2021-12-05 13:00:10 +0000

    Fixed typo in model encoder test

--- model/util.ts
+++ model/util.ts
@@ -1,36 +1,34 @@
 import { IGameState } from "./GameState";
 
-export function printGameState(state: IGameState) {
-    console.log(`
+export function printGameState(state: IGameState): void {
+  console.log(`
         Cash: ${state.cash}, 
         Bike: ${state.bike}, 
         Level: ${state.level}, 
         Beaten[Vermont,Arizona,Tennessee,Hawaii,Alaska]=[${state.beatenLevelVermont},${state.beatenLevelArizona},${state.beatenLevelTennessee},${state.beatenLevelHawaii},${state.beatenLevelAlaska}]
-    `,
-    );
+    `);
 }
 
-
 export function decodePasscodeCharacter(pcode: string): number {
-    let val = 0;
+  let val = 0;
 
-    if ( pcode < 'A' ) {
-        val = pcode.charCodeAt(0) - '0'.charCodeAt(0);
-    } else {
-        val = pcode.charCodeAt(0) - 'A'.charCodeAt(0) + 10;
-    }
+  if (pcode < "A") {
+    val = pcode.charCodeAt(0) - "0".charCodeAt(0);
+  } else {
+    val = pcode.charCodeAt(0) - "A".charCodeAt(0) + 10;
+  }
 
-    return val;
+  return val;
 }
 
 export function encodePasscodeCharacter(val: number): string {
-    let c = '';
+  let c = "";
 
-    if ( val < 10 ) {
-        c = String.fromCharCode( '0'.charCodeAt(0) + val );
-    } else {
-        c = String.fromCharCode( 'A'.charCodeAt(0) + val - 10 );
-    }
+  if (val < 10) {
+    c = String.fromCharCode("0".charCodeAt(0) + val);
+  } else {
+    c = String.fromCharCode("A".charCodeAt(0) + val - 10);
+  }
 
-    return c;
+  return c;
 }
--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
-// module.exports = {
-//     basePath: 'https://ryandev.github.io/RoadRash2-PasscodeGenerator/',
-//     assetPrefix: 'https://ryandev.github.io/RoadRash2-PasscodeGenerator/RoadRash2-PasscodeGenerator/',
-// }
 
-// const isProd = process.env.NODE_ENV === 'production';
-const isProd = false;
-const githubRepoName = 'RoadRash2-PasscodeGenerator';
-const githubAccountName = 'ryandev';
+const env = process?.env ?? {};
 
+const isProd = env.NODE_ENV === 'production';
+const githubRepoName = env.PUBLISH_GHREPONAME || 'RoadRash2-PasscodeGenerator';
+const githubAccountName = env.PUBLISH_GHREPOACCOUNT ||'ryandev';
 
-// module.exports = {
-//   assetPrefix: isProd ? `/${githubRepoName}/` : '',
-//   basePath: isProd ? `/https://${githubAccountName}/${githubRepoName}/` : '',
-// };
 
 module.exports = {
-    assetPrefix: '',
-}
+  assetPrefix: isProd ? `/${githubRepoName}/` : '',
+  basePath: isProd ? `/https://${githubAccountName}/${githubRepoName}/` : '',
+};
