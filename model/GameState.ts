c91878a47186c153a21ebd58780bae66342a7600
Ryan Powell <ryangarethpowell@gmail.com>
2021-12-05 13:28:09 +0000

    Updated indentation

--- model/GameState.ts
+++ model/GameState.ts
@@ -1,10 +1,10 @@
 export interface IGameState {
-    level: number;
-    cash: number;
-    bike: number;
-    beatenLevelAlaska: boolean;
-    beatenLevelHawaii: boolean;
-    beatenLevelTennessee: boolean;
-    beatenLevelArizona: boolean;
-    beatenLevelVermont: boolean;
+  level: number;
+  cash: number;
+  bike: number;
+  beatenLevelAlaska: boolean;
+  beatenLevelHawaii: boolean;
+  beatenLevelTennessee: boolean;
+  beatenLevelArizona: boolean;
+  beatenLevelVermont: boolean;
 }
