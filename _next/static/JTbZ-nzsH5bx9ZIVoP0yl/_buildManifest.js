98e07e303322927ea60c2589363b48523b51fd0f
Ryandev <Ryandev@users.noreply.github.com>
2022-03-15 23:09:16 +0000

    deploy: dd7ab432a586f9b45bb1779ef438e9cd7004a465

---
+++ _next/static/JTbZ-nzsH5bx9ZIVoP0yl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/957-654f14e0aa1c5c80.js","static/chunks/pages/index-97ba2c297ef918ec.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
